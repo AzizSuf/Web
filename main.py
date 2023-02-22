e99ccdaff7ddf095470fea29b86b85cfe5c4c9d5
Aziz Sufyanov <aziz-sufyanov@mail.ru>
2023-02-22 14:45:09 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+
+
+app = Flask(__name__)
+
+
+value = 0
+
+@app.route('/')
+def index():
+    global value
+    val = value
+    value += 1
+
+    return f'The page was loaded {val} times'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About</h1>'
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
